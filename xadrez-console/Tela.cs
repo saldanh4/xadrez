a49cb1b262604d4e31c73e4f56319b4f5f13b4f7
André Saldanha <asaldanha.andre@gmail.com>
2022-11-10 00:04:15 -0300

    Alterado tela para representar o tabuleiro com escala de cores

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -6,21 +6,41 @@ namespace xadrez
     {
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+            
+                
+            
+            Console.BackgroundColor = fundoOriginal;
+            int x = 0;
             for(int i = 0; i < tab.linhas; i++)
             {
                 System.Console.Write(tab.linhas - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
+                    if(x%2 == 0)
+                    {
+                        Console.BackgroundColor = fundoAlterado;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+                    
                     if (tab.peca(i, j) == null)
                     {
-                        System.Console.Write("- ");
+                        System.Console.Write("  ");
                     }
                     else
                     {
                         System.Console.Write(tab.peca(i, j) + " ");
                     }
+                    x++;
                 }
+                x--;
+                Console.BackgroundColor = fundoOriginal;
                 System.Console.WriteLine();
+                
             }
         }
     }
